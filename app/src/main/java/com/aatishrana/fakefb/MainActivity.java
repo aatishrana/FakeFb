abc8a491d69afabb58024acc7d2d722c168b0799
Aatish <aatishrana8@gmail.com>
2017-11-12 14:48:06 +0530

    basic layout

--- app/src/main/java/com/aatishrana/fakefb/MainActivity.java
+++ app/src/main/java/com/aatishrana/fakefb/MainActivity.java
@@ -1,15 +1,121 @@
 package com.aatishrana.fakefb;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.aatishrana.fakefb.fragments.ForthFragment;
+import com.aatishrana.fakefb.fragments.NewsFeed;
+import com.aatishrana.fakefb.fragments.ThirdFragment;
+import com.aatishrana.fakefb.fragments.TwoFragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity
 {
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        toolbar.setTitle("");
+        toolbar.setTitleMargin(0, 0, 0, 0);
+        toolbar.setSubtitle("");
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setSelectedTabIndicatorHeight(0);
+        tabLayout.setupWithViewPager(viewPager);
+        setupTabIcons();
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener()
+        {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab)
+            {
+                tab.setIcon(R.drawable.ic_sample_white);
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab)
+            {
+                tab.setIcon(R.drawable.ic_sample);
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab)
+            {
+
+            }
+        });
+    }
+
+    private void setupTabIcons()
+    {
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_sample_white);
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_sample);
+        tabLayout.getTabAt(2).setIcon(R.drawable.ic_sample);
+        tabLayout.getTabAt(3).setIcon(R.drawable.ic_sample);
+    }
+
+    private void setupViewPager(ViewPager viewPager)
+    {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new NewsFeed(), "");
+        adapter.addFragment(new TwoFragment(), "");
+        adapter.addFragment(new ThirdFragment(), "");
+        adapter.addFragment(new ForthFragment(), "");
+        viewPager.setAdapter(adapter);
+    }
+
+    private class ViewPagerAdapter extends FragmentPagerAdapter
+    {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager)
+        {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position)
+        {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount()
+        {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title)
+        {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position)
+        {
+            return mFragmentTitleList.get(position);
+        }
     }
 }
